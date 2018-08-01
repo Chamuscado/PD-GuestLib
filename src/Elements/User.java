b3a6792bc19c1dc33be01e52fd5388e3d6e13e08
João Leitão <a21250715@isec.pt>
2018-08-01 17:40:55 +0100

    Package Interfaces criado

--- src/Elements/User.java
+++ src/Elements/User.java
@@ -6,10 +6,12 @@ public class User implements Serializable {
     public String username;
     public String name;
     public String password;
+    public boolean parformado;
 
     public User(String name, String username, String password) {
         this.name = name;
         this.password = password;
         this.username = username;
+        parformado = false;
     }
 }
---
+++ src/Interfaces/IGestServerRmi.java
@@ -0,0 +1,19 @@
+package Interfaces;
+
+import Elements.User;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.List;
+
+public interface IGestServerRmi extends Remote {
+    boolean registUser(String name, String username, String password) throws RemoteException;
+
+    boolean login(String username, String password) throws RemoteException;
+
+    List<User> getLoginUsers() throws RemoteException;
+
+    boolean creatPair(User user0, User user1) throws RemoteException;
+
+
+}
