0add4dea15593b4950d995a4b5a2f34df3cea77d
João Leitão <a21250715@isec.pt>
2018-08-01 22:39:36 +0100

    createPair changed

--- src/Interfaces/IGestServerRmi.java
+++ src/Interfaces/IGestServerRmi.java
@@ -8,13 +8,14 @@ import java.util.List;
 
 public interface IGestServerRmi extends Remote {
     String ServiceName = "PDGuestServer";
+
     boolean registUser(String name, String username, String password) throws RemoteException;
 
     boolean login(String username, String password) throws RemoteException;
 
     List<User> getLoginUsers() throws RemoteException;
 
-    boolean creatPair(User user0, User user1) throws RemoteException;
+    boolean creatPair(String user0, String user1) throws RemoteException;
 
 
 }
